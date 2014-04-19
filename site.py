34803220bcbc09f3c6147eb8d8334cdb8cea6258
Will Drevo <will.drevo@gmail.com>
2014-04-19 15:36:23 -0400

    more on getting add groups

--- site.py
+++ site.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from marathon.scrape import get_name_matches
 from marathon.database import *
+from flask import request
+import json
 
 app = Flask(__name__)
 
@@ -10,15 +12,22 @@ DEBUG = True
 @app.route('/')
 def index():
 	return render_template('index.html', message="Start tracking your friends!")
-    
-@app.route('/<groupname>')
+
+@app.route('/<groupname>', methods=["POST", "GET"])
 def show_grouppage(groupname):
 	runners = []
 	if groupname == "stats":
 		return render_template('stats.html')
+	elif groupname == "addgroup":
+		first = request.form.get("first", "")
+		last = request.form.get("last", "")
+		matches = get_name_matches(first, last)
+		return render_template('addgroup.html', matches=json.dumps(matches))
 	else:
-		runners = GetGroupMembers(groupname)
-		return render_template('group.html', groupname=groupname, runners=runners)
+		first = request.form.get("first", "")
+		last = request.form.get("last", "")
+		matches = get_name_matches(first, last)
+		return render_template('api.html', matches=json.dumps(matches))
 
 if __name__ == '__main__':
     app.run(debug=DEBUG)
